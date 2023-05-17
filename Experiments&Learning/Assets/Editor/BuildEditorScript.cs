0335f70e203f6ee78e33b601f482fc4ab92547f0
unframed3301 <dEHwabXqLPnFPs7iWUjbU2rEtKSXQXXpYXcZdEqF6xABbgpf8yUK7aoSD2kgkvfE@tuta.io>
2023-05-17 19:20:58 +0200

    Automation build class for Win64

---
+++ Experiments&Learning/Assets/Editor/BuildEditorScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System;
+using UnityEditor;
+
+class BuildEditorScript
+{
+    static string[] SCENES = FindEnabledEditorScenes();
+
+    static string APP_NAME = "OnlyWantedBeer";
+    static string TARGET_DIR = "Win64";
+
+    [MenuItem("Custom/CI/Build Win64")]
+    static void Build()
+    {
+        string target_dir = APP_NAME + ".exe";
+        GenericBuild(SCENES, TARGET_DIR + "/" + target_dir, BuildTarget.StandaloneWindows64, BuildOptions.None);
+    }
+
+    private static string[] FindEnabledEditorScenes()
+    {
+        List<string> EditorScenes = new List<string>();
+        foreach (EditorBuildSettingsScene scene in EditorBuildSettings.scenes)
+        {
+            if (!scene.enabled) continue;
+            EditorScenes.Add(scene.path);
+        }
+        return EditorScenes.ToArray();
+    }
+
+    static void GenericBuild(string[] scenes, string target_dir, BuildTarget build_target, BuildOptions build_options)
+    {
+        EditorUserBuildSettings.SwitchActiveBuildTarget(build_target);
+        var res = BuildPipeline.BuildPlayer(scenes, target_dir, build_target, build_options);
+    }
+}
